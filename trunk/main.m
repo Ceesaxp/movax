435c48db9a449caf7835c21589695f8d358ca4a5
ceesaxp <ceesaxp@18e97933-0d1a-0410-bf3d-dbc42dd76b11>
2006-08-04 04:21:59 +0000

    Initial import

---
+++ trunk/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  MovaX
+//
+//  Created by Andrei Popov on 7/24/06.
+//  Copyright (c) 2006 __MyCompanyName__. All rights reserved.
+//
+
+#import <RubyCocoa/RBRuntime.h>
+
+int main(int argc, const char *argv[])
+{
+    return RBApplicationMain("rb_main.rb", argc, argv);
+}
